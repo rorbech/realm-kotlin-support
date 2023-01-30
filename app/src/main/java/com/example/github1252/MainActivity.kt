c1be0e5f91e9d698284ac2ad1952d93c3aa61abc
Claus Rørbech <claus.rorbech@mongodb.com>
2023-01-30 15:42:31 +0100

    Add realm usage

--- app/src/main/java/com/example/github1252/MainActivity.kt
+++ app/src/main/java/com/example/github1252/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.github1252
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import io.realm.kotlin.Realm
+import io.realm.kotlin.RealmConfiguration
+import io.realm.kotlin.ext.query
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val realm = Realm.open(RealmConfiguration.create(setOf(Sample::class)))
+        realm.writeBlocking { copyToRealm(Sample()) }
+
+        Log.d("HILTTEST", "${realm.query<Sample>().find().size}")
     }
 }
---
+++ app/src/main/java/com/example/github1252/Sample.kt
@@ -0,0 +1,7 @@
+package com.example.github1252
+
+import io.realm.kotlin.types.RealmObject
+
+class Sample: RealmObject {
+    var id: String = "Sample"
+}
